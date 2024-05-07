43e4850ddc9e935ed78036ff877404cfe112f5a2
StephenFeddes <feddessteve@gmail.com>
2024-05-07 14:56:42 -0500

    Troubleshooting player fetcher.

--- src/presentation/routes/players_router.py
+++ src/presentation/routes/players_router.py
@@ -93,6 +93,50 @@ async def testing():
         return Response(status_code=500, content=str(e))
 
 
+@players_router.get("/api/v1/testing5")
+async def testing():
+    try:
+        teams_dict: dict = {}
+        for team in teams_fetcher.get_teams():
+            teams_dict[team["abbreviation"]] = TeamEntity(
+                teamId=str(team["id"]), location=team["city"], name=team["nickname"], abbreviation=team["abbreviation"]
+            )
+
+        nba_api_players: list[dict] = nba_api_players_fetcher.get_players()
+
+        # Get the NBA players from the Sleeper API
+        sleeper_api_players: dict = requests.get(url="https://api.sleeper.app/v1/players/nba").json()
+
+        player_name_to_sleeper_api_id_dict: dict = {
+            player_data.get("full_name", player_id): player_id
+            for player_id, player_data in sleeper_api_players.items()
+            if player_data.get("status") == "ACT"
+        }
+
+        # Get the player's projected season stats and rankings for points and category leagues
+        season_projections_fetcher = PlayersSeasonProjectionsFetcher()
+        players_season_projections: dict[str, dict] = season_projections_fetcher.fetch_projections()
+
+        # Get the players that are currently being added the most in Sleeper's fantasy app
+        ADDS_URL: str = "https://api.sleeper.app/v1/players/nba/trending/add?limit=50"
+        await asyncio.sleep(5)  # Sleep for 5 seconds to avoid rate limiting
+        player_adds_dict: dict = {record["player_id"]: record["count"] for record in requests.get(ADDS_URL).json()}
+
+        # Get the players that are currently being dropped the most in Sleeper's fantasy app
+        DROPS_URL: str = "https://api.sleeper.app/v1/players/nba/trending/drop?limit=50"
+        await asyncio.sleep(5)  # Sleep for 5 seconds to avoid rate limiting
+        player_drops_dict: dict = {record["player_id"]: record["count"] for record in requests.get(DROPS_URL).json()}
+
+        return {
+            "player_drop_dict": player_drops_dict,
+            "player_add_dict": player_adds_dict,
+            "player_name_to_sleeper_api_id_dict": player_name_to_sleeper_api_id_dict,
+            "players_season_projections": players_season_projections,
+        }
+
+    except Exception as e:
+        return Response(status_code=500, content=str(e))
+
 @players_router.get("/api/v1/testing2")
 async def testing():
     try:
